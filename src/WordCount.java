c1bb5b0efd7bd4cae3a10104686f1ebfebe0cd44
xuyaaaaaaaaaaabi <1241859128@qq.com>
2021-03-04 19:43:02 +0800

    1.11v 相对路径

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,9 +1,12 @@
+import java.io.File;
 import java.io.IOException;
 import workFunc.working_set;
 
 public class WordCount {
 	public static void main(String[] args) throws IOException {
 		//调用统计单词数的函数
+		working_set.inPath = new File("1.txt").getAbsolutePath();
+		working_set.outPath = new File("2.txt").getAbsolutePath();
 		working_set.getNumberOfWords();
 	}
 }
