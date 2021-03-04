8df52c6c29d6006e36dfa32051c0babbd618eef5
xuyaaaaaaaaaaabi <1241859128@qq.com>
2021-03-04 17:29:40 +0800

    1.10v

--- src/WordCount.java
+++ src/WordCount.java
@@ -3,7 +3,7 @@ import workFunc.working_set;
 
 public class WordCount {
 	public static void main(String[] args) throws IOException {
-		//调用统计单词数函数
+		//调用统计单词数的函数
 		working_set.getNumberOfWords();
 	}
 }
